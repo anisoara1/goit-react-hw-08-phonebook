a139b21aef0ce31704038465488d3ec34048f137
anisoara1 <122683089+anisoara1@users.noreply.github.com>
2023-10-06 07:24:23 +0000

    Deploying to gh-pages from @ anisoara1/goit-react-hw-08-phonebook@c57a3bf3323e1679123f509ca1b781634cb054c7 🚀

---
+++ static/js/266.5f478b6d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{266:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var i=n(184),o={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function c(){return(0,i.jsx)("div",{style:o.container,children:(0,i.jsxs)("h1",{style:o.title,children:["Phonebook welcome page"," "]})})}}}]);
+//# sourceMappingURL=266.5f478b6d.chunk.js.map
